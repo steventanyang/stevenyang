4145b5315dc7965f4aae0fe90069221a6ffdaf58
stevenyang <steventanyang@gmail.com>
2024-01-17 23:10:20 -0500

    added on click grey box

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,13 @@ import '../static/Projects.css'
 import Toggle from '../components/Toggle'
 
 import styled from 'styled-components'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { DarkContext } from '../contexts/DarkContext'
 import { Link } from 'react-router-dom';
+import legm from '../images/legm.png'
+import projectlebron from '../images/projectlebron.png'
+import asklebron from '../images/asklebron.png'
+import pokemon from '../images/pokemon.png'
 
 const PageWrap = styled.div`
   background-color: ${props => props.theme.backgroundColor};
@@ -39,6 +43,15 @@ export default function Projects() {
 
   const { theme, setTheme, isToggled, setIsToggled } = useContext(DarkContext);
 
+    const [selectedImage, setSelectedImage] = useState({});
+
+    const handleClick = (imageId) => {
+      setSelectedImage(prevSelected => ({
+          ...prevSelected,
+          [imageId]: !prevSelected[imageId]
+      }));
+    };
+
   return (
     <PageWrap>
 
@@ -71,16 +84,56 @@ export default function Projects() {
       {/* 2024 */}
       <div className='subsoil' style={{ backgroundColor: theme === 'light' ? '#654A15': '#3D4334'}}>
         <h2 className='yeartitle' style={{ color: theme === 'light' ? '#826122': '#576151' }}>2024</h2>
+          
+        <div className='projects-container'>
+          <div className='image-container' onClick={() => handleClick('legm')}>
+              {selectedImage['legm'] 
+                  ? <div className='grey-overlay'></div>
+                  : <img className='project-box' src={legm} style={{ marginLeft: 'none' }}/>
+              }
+          </div>
+        </div>
+
       </div>
 
       {/* 2023 */}
       <div className='subsoil' style={{ backgroundColor: theme === 'light' ? '#4C370D': '#2C3023'}}>
         <h2 className='yeartitle' style={{ color: theme === 'light' ? '#654A15': '#464D39' }}>2023</h2>
+
+        <div className='projects-container'>
+            <div className='image-container' onClick={() => handleClick('projectlebron')}>
+                {selectedImage['projectlebron']
+                    ? <div className='grey-overlay'></div>
+                    : <img className='project-box' src={projectlebron} />
+                }
+            </div>
+            <div className='image-container' onClick={() => handleClick('projectlebron')}>
+                {selectedImage['projectlebron']
+                    ? <div className='grey-overlay'></div>
+                    : <img className='project-box' src={projectlebron} />
+                }
+            </div>
+        </div>
       </div>
 
       {/* 2022 */}      
       <div className='subsoil' style={{ backgroundColor: theme === 'light' ? '#726240': '#4C5046'}}>
         <h2 className='yeartitle' style={{ color: theme === 'light' ? '#4C370D': '#2C3023' }}>2022</h2>
+
+        <div className='projects-container'>
+          <div className='image-container' onClick={() => handleClick('asklebron')}>
+            {selectedImage['asklebron']
+                ? <div className='grey-overlay'></div>
+                : <img className='project-box' src={projectlebron} style={{ marginLeft: 'none' }}/>
+            }
+          </div>
+          <div className='image-container' onClick={() => handleClick('pokemon')}>
+            {selectedImage['pokemon']
+                ? <div className='grey-overlay'></div>
+                : <img className='project-box' src={pokemon} />
+            }
+          </div>
+        </div>
       </div>
 
       {/* <div className='test-container'>
@@ -88,8 +141,6 @@ export default function Projects() {
           <h2>penis</h2>
         </Link>
       </div> */}
-
-
 
     </PageWrap>
   );
